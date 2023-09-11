fb3d8d0407e05d75d78f0abd36e4dacf3faa57f4
Lisettellanos02 <llanoslisette215@gmail.com>
2023-09-11 17:32:45 -0300

    actualizo cambios en 2da preentrega

--- index.js
+++ index.js
@@ -1,83 +1,26 @@
-/*class TiketManager {
-    #precioBaseDeGanancia = 2
-    constructor(){
-        this.eventos = []
+const products = [
+    {
+        name: 'Iphone',
+        price: 500,
+        stcok: 25
+    },
+    {
+        name: 'Ipad',
+        price: 200,
+        stcok: 35
+    },
+    {
+        name: 'TV',
+        price: 1500,
+        stcok: 15
+    },
+    {
+        name: 'Galaxy',
+        price: 400,
+        stcok: 4
     }
-    agregarEvento(nombre,lugar,precio,capacidad=50,fecha= new Date()){
-    const evento = {
-        id: this.#generarIdEvento(),
-        nombre,
-        lugar,
-        precio: precio + this.#precioBaseDeGanancia,
-        capacidad,
-        fecha,
-        participantes: []
-    }
-    this.eventos.push(evento)
-}
+]
 
-agregarUsuario(idEvento, idUsuario){
-    const evento = this.#buscarEvento(idEvento)
-    if(!evento){
-        return 'Este producto no existe'
-    }
-    if(evento.participantes.includes(idUsuario)){
-        return 'Usuario ya registrado en el evento'
-    }
-    evento.participantes.push(idUsuario)
-}
-ponerEventoEnGira(idEvento,nuevoLugar,nuevaFecha){
-    const evento = this.#buscarEvento(idEvento)
-    if(!evento){
-        return 'Este producto no existe'
-    }
-    const nuevoEvento = {
-        ...evento,
-        lugar: nuevoLugar,
-        fecha: nuevaFecha,
-        participantes: [],
-        id: this.#generarIdEventos()
-    }
-    this.eventos.push(nuevoEvento)
-}
-
-   #buscarEvento(idEvento){
-    return this.eventos.find(e=>e.id === idEvento)
-  }
-   #generarIdEventos(){
-    return this.eventos.length
-    ? this.eventos[this.eventos.length-1].id+1
-    : 1
-  }
-
-}*/
-//SINCRONICA
-const fs = requiere('fs')
-
-//escribir el archivo
-//fs.writeFileSync('archivo.txt', 'primer archivo creado')
-
-//leer un archivo
-//const infoArchivo = fs.readFileSync('archivo.txt')
-//console.log(infoArchivo);
-
-//eliminar un archivo
-//fs.unlinkSync('archivo.txt')
-
-//existe un archivo
-//const existeArchivo = fs.existsSync ('archivo.txt')
-//console.log(existeArchivo);
-
-//añadir informacion
-//fs.appendFileSync('archivo.txt' , 'segundo parrafo')
-
-//ASINCRONICA
-
-//escribir el archivo
-fs.writeFile('archivoAsinc.txt' , 'primer archivo', (error)=>{
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Archivo creado con exito');
-    }
-})
+fs.promises.readFile('products.json','utf-8')
+.then(info=>console.log(JSON.parse(info)))
+.catch(error=>console.log(error))
