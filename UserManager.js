9e5018b6ddd88628dc48ebc9bef8e5f621569ecf
Lisettellanos02 <llanoslisette215@gmail.com>
2023-09-22 18:22:31 -0300

    actualizo cambios para 4ta preentrega

--- UserManager.js
+++ UserManager.js
@@ -1,102 +1,87 @@
-const fs = require('fs')
+import fs from 'fs'
 
-
-class UserManager{
-
-    constructor(path){
-        this.path = path
+class UsersManager {
+  constructor(path) {
+    this.path = path
+  }
+  async getUsers(queryObj) {
+    const { order } = queryObj
+    try {
+        if (fs.existsSync(this.path)) {
+          console.log('order',order);
+        const info = await fs.promises.readFile(this.path, 'utf-8')
+        const infoParsed = JSON.parse(info)
+        return order === 'ASC'
+          ? infoParsed.sort((a, b) => a.first_name.localeCompare(b.first_name))
+          : order === 'DESC'
+          ? infoParsed.sort((a, b) => b.first_name.localeCompare(a.first_name))
+          : infoParsed
+      } else {
+        return []
+      }
+    } catch (error) {
+      return error
     }
-    async getUsers(){
-        try {
-            if(fs.existsSync(this.path)){
-                const info = await fs.promises.readFile(this.path,'utf-8')
-                return JSON.parse(info)
-            } else {
-                return []
-            }
-        } catch (error) {
-            return error
-        }
+  }
+
+  async createUser(obj) {
+    try {
+      const users = await this.getUsers({})
+      let id
+      if (!users.length) {
+        id = 1
+      } else {
+        id = users[users.length - 1].id + 1
+      }
+      const newUser = { id, ...obj }
+      users.push(newUser)
+      await fs.promises.writeFile(this.path, JSON.stringify(users))
+      return newUser
+    } catch (error) {
+      return error
     }
+  }
 
-    async createUser(obj){
-        try {
-            const users = await this.getUsers()
-            let id 
-            if(!users.length){
-                id = 1
-            } else {
-                id = users[users.length-1].id+1
-            }
-            users.push({id,...obj})
-            await fs.promises.writeFile(this.path,JSON.stringify(users))
-
-        } catch (error) {
-            return error
-        }
+  async getUserById(idUser) {
+    try {
+      const users = await this.getUsers({})
+      const user = users.find((u) => u.id === idUser)
+      return user
+    } catch (error) {
+      return error
     }
+  }
 
-
-    async getUserById(idUser){
-        try {
-            const users = await this.getUsers()
-            const user = users.find(u=>u.id === idUser)
-            if(user){
-                return user
-            } else {
-                return 'No user'
-            }
-        } catch (error) {
-            return error
-        }
-
+  async deleteUser(idUser) {
+    try {
+      const users = await this.getUsers()
+      const user = users.find((u) => u.id === idUser)
+      if (!user) {
+        return -1
+      }
+      const newArrayUsers = users.filter((u) => u.id !== idUser)
+      await fs.promises.writeFile(this.path, JSON.stringify(newArrayUsers))
+      return 1
+    } catch (error) {
+      return error
     }
+  }
 
-    async deleteUser(idUser){
-        try {
-            const users = await this.getUsers()
-            const newArrayUsers = users.filter(u=>u.id!==idUser)
-            await fs.promises.writeFile(this.path,JSON.stringify(newArrayUsers))
-        } catch (error) {
-            return error
-        }
+  async updateUser(idUser, obj) {
+    try {
+      const users = await this.getUsers()
+      const index = users.findIndex((u) => u.id === idUser)
+      if (index === -1) {
+        return -1
+      }
+      const user = users[index]
+      users[index] = { ...user, ...obj }
+      await fs.promises.writeFile(this.path, JSON.stringify(users))
+      return 1
+    } catch (error) {
+      return error
     }
-
+  }
 }
 
-const user1 = {
-    first_name: 'Laura',
-    last_name: 'Suarez',
-    age: 25,
-    course: 'JavaScript'
-}
-
-const user2 = {
-    first_name: 'Juan',
-    last_name: 'Suarez',
-    age: 25,
-    course: 'JavaScript'
-}
-
-const user3 = {
-    first_name: 'Maria',
-    last_name: 'Suarez',
-    age: 25,
-    course: 'JavaScript'
-}
-
-const user4 = {
-    first_name: 'Pedro',
-    last_name: 'Suarez',
-    age: 25,
-    course: 'JavaScript'
-}
-
-
-async function test(){
-    const manager1 = new UserManager('Users.json')
-    await manager1.deleteUser(4)
-    const users = await manager1.getUsers()
-    console.log(users);
-}
-test()
+export const usersManager = new UsersManager('UsersAPI.json')
